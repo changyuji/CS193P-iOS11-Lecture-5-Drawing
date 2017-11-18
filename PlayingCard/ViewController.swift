20c178dd5f2f7e32cb1ec4c7522cd8223605fcb5
Michel Deiman <michel.deiman@live.nl>
2017-11-18 14:42:19 +0100

    All coding for lecture 5 as demonstrated by the lecturer.

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var deck = PlayingCardDeck()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+		
+		var card = deck.draw()
+		while card != nil {
+			print("\(card!)")
+			card = deck.draw()
+		}
 	}
 
 
